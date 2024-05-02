28d144da9a96c970783b3e01fc2757c492f1bec6
sanghrakshit12 <gautamsanghrakshit@gmail.com>
2024-05-02 08:33:00 +0530

    Reapet/Missing

---
+++ Array Part-2/Repeating_Missing.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// vector<int> Brute(vector<int> nums)
+// {
+//     int n = nums.size();
+//     int r=-1,m=-1;
+//     for (int i = 1; i <=n; i++){
+//         int c = 0;
+//         for (int j = 0; j < n; j++){
+//             if (i == nums[j])
+//                 c++;
+//         }
+//         if (c == 2)
+//              r = i;
+//         if (c == 0)
+//              m = i;
+//     }
+//     return {r,m};
+// }
+
+vector<int> Better(vector<int>&nums){
+
+}
+
+int main()
+{
+    cout << "Enter size" << endl;
+    int n;
+    cin >> n;
+    vector<int> nums;
+    for (int i = 0; i < n; i++)
+    {
+        int a;
+        cin >> a;
+        nums.push_back(a);
+    }
+    cout << "initial Matrix" << endl;
+    for (auto i : nums)
+    {
+        cout << i << " ";
+    }
+    cout << endl;
+    vector<int>res=Better(nums);
+    for(auto i:res){
+        cout<<i<<" ";
+    }
+}
