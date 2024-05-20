d6e09c54f3d81944b475cab0bd7be66e1a34d6b8
sanghrakshit12 <gautamsanghrakshit@gmail.com>
2024-05-21 02:18:37 +0530

    RotationNumber

--- BinarySearch/RotationCount.cpp
+++ BinarySearch/RotationCount.cpp
@@ -3,12 +3,49 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int Rotation(vector<int> nums)
-{
+
+// TC->O(N)
+// int Brute(vector<int> nums){
+//     int n = nums.size();
+//     for (int i = 0; i < n - 1; i++){
+//         if (nums[i] > nums[i + 1])
+//             return i + 1;
+//     }
+//     return 0;
+// }
+
+// TC->O(logN)
+int Rotation(vector<int> nums){
+    int n = nums.size();
+    int l = 0, h = n - 1, mini = INT_MAX, res = 0;
+    while (l <= h){
+        int m = l + (h - l) / 2;
+        if (nums[l] <= nums[h]){
+            if (mini > nums[l]){
+                mini = nums[l];
+                res = l;
+            }
+            break;
+        }
+        if (nums[l] <= nums[m]){
+            if (nums[l] < mini){
+                res = l;
+                mini = nums[l];
+            }
+            l = m + 1;
+        }
+        else{
+            if (nums[m] < mini){
+                res = m;
+                mini = nums[m];
+            }
+            h = m - 1;
+        }
+    }
+    return res;
 }
 
-int main()
-{
+int main(){
     cout << "Enter size" << endl;
     int n;
     cin >> n;
