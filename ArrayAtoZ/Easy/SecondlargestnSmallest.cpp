2ecaac4d3e22753632fa0be2b5210e4bf9a0eb2f
Sanghrakshit12 <gautamsanghrakshit@gmail.com>
2024-06-12 15:18:36 +0530

    update

--- ArrayAtoZ/Easy/SecondlargestnSmallest.cpp
+++ ArrayAtoZ/Easy/SecondlargestnSmallest.cpp
@@ -4,30 +4,10 @@ using namespace std;
 
 int SecondLargest(vector<int> nums)
 {
-    int n = nums.size();
-    int mx = nums[0], sm = -1;
-    for (int i = 1; i < n; i++)
-    {
-        if (nums[i] > mx)
-        {
-            sm = mx;
-            mx = nums[i];
-        }
-    }
 }
 
 int SecondSmallest(vector<int> nums)
 {
-    int n = nums.size();
-    int mx = nums[0], sm = -1;
-    for (int i = 1; i < n; i++)
-    {
-        if (nums[i] > mx)
-        {
-            sm = nums[i];
-            mx 
-        }
-    }
 }
 
 int main()
