8f2517bfa36aeb699e2c2b493841abfef142d6b2
Sanghrakshit12 <gautamsanghrakshit@gmail.com>
2024-06-18 02:10:22 +0530

    Subarr Sum

--- ArrayAtoZ/Easy/LongestSubarrSumK.cpp
+++ ArrayAtoZ/Easy/LongestSubarrSumK.cpp
@@ -41,27 +41,48 @@ using namespace std;
 // }
 
 // Using Hashing
-int Better(vector<int>nums,int K){
-    unordered_map<int,int>mpp;
-    int s=0;
-    int n=nums.size();
-    for(int i=0;i<n;i++){
-        s+=nums[i];
-        mpp[i]=s;
-    }
-    s=0;
-    for(int i=0;i<n;i++){
-        s+=nums[i];
-        if(mpp.find(K-s)!=mpp.end()){
+// int Better(vector<int> nums, int K){
+//     unordered_map<int, int> mpp;
+//     int n = nums.size();
+//     int sum = 0, res = 0;
+//     for (int i = 0; i < n; i++){
+//         sum += nums[i];
+//         if (sum == K)
+//             res = max(res, i + 1);
+//         if (mpp.find(sum - K) != mpp.end())
+//             res = max(res, i - mpp[sum - K]);
+//         if (mpp.find(sum) == mpp.end())
+//             mpp[sum] = i;
+//     }
+//     return res;
+// }
 
+
+// Optimal Soln 2 pointer
+int Optimal(vector<int> &a, long long k){
+    int n = a.size();
+    int left = 0, right = 0;
+    long long sum = a[0];
+    int maxLen = 0;
+    while (right < n){
+        while (left <= right && sum > k){
+            sum -= a[left];
+            left++;
         }
+        if (sum == k)
+            maxLen = max(maxLen, right - left + 1);
+        right++;
+        if (right < n)
+            sum += a[right];
     }
+    return maxLen;
 }
+
 
 int main()
 {
     vector<int> nums = {10, 5, 2, 7, 1, 9};
     int k;
     cin >> k;
-    cout << Better(nums, k);
+    cout << Optimal(nums, k);
 }
