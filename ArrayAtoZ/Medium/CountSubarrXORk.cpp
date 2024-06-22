1fd6c333ef27d9da8cd3d0ef99bf2b5e37a08eef
Sanghrakshit12 <gautamsanghrakshit@gmail.com>
2024-06-22 13:53:55 +0530

    Subarr

--- ArrayAtoZ/Medium/CountSubarrXORk.cpp
+++ ArrayAtoZ/Medium/CountSubarrXORk.cpp
@@ -33,9 +33,18 @@ using namespace std;
 //     return res;
 // }
 
-
 int Optimal(vector<int> nums, int K){
-    
+    int n = nums.size();
+    unordered_map<int, int> mpp;
+    mpp[0] = 1;
+    int x = 0, res = 0;
+    for (int i = 0; i < n; i++){
+        x = x ^ nums[i];
+        if (mpp.find(x ^ K) != mpp.end())
+            res += mpp[x ^ K];
+        mpp[x]++;
+    }
+    return res;
 }
 
 int main()
